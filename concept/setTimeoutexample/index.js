6223d4adeafe7f0c9522b6de219f019cef0d80d4
THAMITHA S <THAMITHA.S@ibm.com>
2024-09-08 12:15:59 +0530

    add setTimeout

---
+++ concept/setTimeoutexample/index.js
@@ -0,0 +1,11 @@
+function greeting(){
+    console.log('Hello World!!!')
+}
+
+function greetingAlien(){
+    console.log('Greeting Alien')
+}
+
+setTimeout(greetingAlien, 3*1000)
+
+// setInterval(greeting, 1000)
