8b96d39e207276305933b9ad5e503b2077513996
THAMITHA S <THAMITHA.S@ibm.com>
2024-09-08 13:07:32 +0530

    print elapsedtime

--- concept/counter/index.js
+++
@@ -1,9 +0,0 @@
-let counter = 30
-const countDown = setInterval(()=>{
-    console.log(counter)
-    counter--
-    if(counter<0){
-        clearInterval(countDown)
-        console.log('counter has fininshed!!!')
-    }
-}, 1000)
---
+++ concept/example/index.js
@@ -0,0 +1,7 @@
+const startTime = Date.now(); 
+
+setTimeout(() => {
+  const endTime = Date.now(); 
+  const elapsedTime = endTime - startTime; 
+  console.log(`Elapsed time: ${elapsedTime} ms`);
+}, 1000);
