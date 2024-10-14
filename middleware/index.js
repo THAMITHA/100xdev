bb442a6a1a34d2716274909dd44d988b1f43aa7b
THAMITHA S <THAMITHA.S@ibm.com>
2024-10-14 20:53:25 +0530

    before middleware

---
+++ middleware/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+app.get('/health-checkup', function(req, res){
+    const username = req.headers.username
+    const password = req.headers.password
+    const kidneyId = req.query.kidneyId 
+    
+    if(!(username == 'harkirat' && password == 'pass')){
+        res.status(400).json({
+            msg: 'something is up in input'
+        })
+        return 
+    }
+    if(kidneyId!=1 && kidneyId!=2){
+        res.status(400).json({
+            msg: 'something is up in input'
+        })
+    }
+    res.json({
+        msg: 'your kidney is fine!'
+    })
+    
+})     
+        
+app.listen(3000)
+
