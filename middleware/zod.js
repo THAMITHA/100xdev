59370433e0bffc39b5f3426907c48e242f1e5ab3
THAMITHA S <THAMITHA.S@ibm.com>
2024-10-15 06:58:20 +0530

    added zod example

--- middleware/zod.js
+++ middleware/zod.js
@@ -1,10 +1,16 @@
 const zod = require('zod')
 
-function validateInput(arr){
-    const schema = zod.array(zod.number())
-    const response = schema.safeParse(arr)
+function validateInput(obj){
+    const schema = zod.object({
+        email: zod.string().email(),
+        password: zod.string().min(8)
+    })
+    const response = schema.safeParse(obj)
     console.log(response)
 }
 
-validateInput([1,2,3])
+validateInput({
+    email: 'thamitha@gmail.com',
+    password: 'vdbdfb343'
+})
 
