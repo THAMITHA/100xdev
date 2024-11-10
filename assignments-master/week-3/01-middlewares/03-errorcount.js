7da24a9f31f4daf0c94bdf3af2713a12d7688baa
THAMITHA S <THAMITHA.S@ibm.com>
2024-11-10 11:49:25 +0530

    solve middleware assignments

--- assignments-master/week-3/01-middlewares/03-errorcount.js
+++ assignments-master/week-3/01-middlewares/03-errorcount.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const assert = require('assert');
 const express = require('express');
+const { error } = require('console');
 
 const app = express();
 let errorCount = 0;
@@ -22,5 +23,10 @@ app.post('/user', function(req, res) {
 app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
+
+app.use(function(err, req, res, next){
+  res.status(404).send({})
+  errorCount= errorCount+1
+})
 
 module.exports = app;
