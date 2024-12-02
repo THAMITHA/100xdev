5cb9c0a767947c0b179b9915f49ef867d83c0f75
THAMITHA S <THAMITHA.S@ibm.com>
2024-12-02 20:24:09 +0530

    todo full stack

---
+++ new-year-app/backend/db.js
@@ -0,0 +1,27 @@
+/*
+Todo{
+title: string,
+description: string,
+completed: boolean
+}
+
+*/
+
+const mongoose = require('mongoose')
+
+mongoose.connect('')
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const todo = mongoose.model('todos', todoSchema)
+
+module.exports = {
+    todo
+}
