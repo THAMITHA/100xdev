bd65867876c2741c16fc8a383f42fdb730a4319d
THAMITHA S <THAMITHA.S@ibm.com>
2024-12-04 16:33:26 +0530

    add id

--- week-6-expirements/src/App.jsx
+++ week-6-expirements/src/App.jsx
@@ -1,25 +1,38 @@
 import './App.css'
 import React, { useState} from 'react'
-
+let count = 4
 function App() {
-  const [title, setTitle] = useState("my title is thamitha")
-  function updateTitle(){
-    setTitle('My name is '+ Math.random())
+  const [todos, setTodos] =useState([{
+    id: '1',
+    title: 'goto gym',
+    description: 'goto gym from 7-9'  },
+    { id: '2',
+      title: 'learn DSA',
+      description: 'goto gym from 9-12'  },
+      {
+        id: '3',
+        title: 'learn system design',
+        description: 'goto gym from 1-5'  },  
+  ])
+
+  function addTodo(){
+    setTodos([...todos,{
+      id: count++,
+      title: Math.random() ,
+      description: Math.random()
+    }])
   }
   return (<div>
-      <button onClick={updateTitle}>click me to change title</button>
-      <Header title={title}></Header>
-      <Header title="thamitha1"></Header>
-      <Header title="thamitha2"></Header>
+    <button onClick={addTodo}>add new todo</button>
+    {todos.map(todo=><Todo key={todo.id} title={todo.title} description={todo.description}></Todo>
+    )}
     </div>)
 }
 
-const Header = React.memo(function({title}){
-  return(<div>
-    {title}
+function Todo({title, description}){
+  return<div>
+    <h1>{title}</h1>
+    <h5>{description}</h5>
   </div>
-  )
-})
-
-
+}
 export default App
