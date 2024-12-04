9272b0e285d52a499be9d4e7eaac0cd173b9e95d
THAMITHA S <THAMITHA.S@ibm.com>
2024-12-04 12:59:04 +0530

    re-render

--- week-6-expirements/src/App.jsx
+++ week-6-expirements/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [title, setTitle] = useState("thamitha1")
+  function updateTitle(){
+    setTitle('My name is '+ Math.random())
+  }
   return (
     <>
-      <Header title="thamitha1"></Header>
+      <button onClick={updateTitle}>click me to change the title</button> 
+      <Header title={title}></Header>
       <Header title="thamitha2"></Header>
       <Header title="thamitha3"></Header>
       <Header title="thamitha4"></Header>
