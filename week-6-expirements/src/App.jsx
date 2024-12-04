a54387d68443d5f2e9a814f66dd870e8b5f073a4
THAMITHA S <THAMITHA.S@ibm.com>
2024-12-04 15:43:35 +0530

    add React.memo

--- week-6-expirements/src/App.jsx
+++ week-6-expirements/src/App.jsx
@@ -1,29 +1,25 @@
 import './App.css'
-import { useState } from 'react'
+import React, { useState} from 'react'
 
 function App() {
-  return (
-    <HeaderWithButton></HeaderWithButton>
-  )
-}
-
-function HeaderWithButton(){
-  const [title, setTitle] = useState("thamitha1")
+  const [title, setTitle] = useState("my title is thamitha")
   function updateTitle(){
     setTitle('My name is '+ Math.random())
   }
-  return (
-    <div>
-      <button onClick={updateTitle}>click me to change the title</button> 
+  return (<div>
+      <button onClick={updateTitle}>click me to change title</button>
       <Header title={title}></Header>
+      <Header title="thamitha1"></Header>
       <Header title="thamitha2"></Header>
-    </div>
-  )
+    </div>)
 }
 
-function Header({title}){
-  return <div>
+const Header = React.memo(function({title}){
+  return(<div>
     {title}
   </div>
-}
+  )
+})
+
+
 export default App
