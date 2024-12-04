081e638ea1cee508940f72a9f5e0569a8f6bfead
THAMITHA S <THAMITHA.S@ibm.com>
2024-12-04 17:26:06 +0530

    add wrappers

--- week-6-expirements/src/App.jsx
+++ week-6-expirements/src/App.jsx
@@ -1,38 +1,17 @@
-import './App.css'
-import React, { useState} from 'react'
-let count = 4
-function App() {
-  const [todos, setTodos] =useState([{
-    id: '1',
-    title: 'goto gym',
-    description: 'goto gym from 7-9'  },
-    { id: '2',
-      title: 'learn DSA',
-      description: 'goto gym from 9-12'  },
-      {
-        id: '3',
-        title: 'learn system design',
-        description: 'goto gym from 1-5'  },  
-  ])
-
-  function addTodo(){
-    setTodos([...todos,{
-      id: count++,
-      title: Math.random() ,
-      description: Math.random()
-    }])
-  }
-  return (<div>
-    <button onClick={addTodo}>add new todo</button>
-    {todos.map(todo=><Todo key={todo.id} title={todo.title} description={todo.description}></Todo>
-    )}
-    </div>)
+function App({InlineComponent}){
+  return <div>
+    <CardWrapper InlineComponent={<TextComponent/>}/>
+  </div>  
 }
 
-function Todo({title, description}){
-  return<div>
-    <h1>{title}</h1>
-    <h5>{description}</h5>
+function CardWrapper({InlineComponent}){
+  return <div style={{border: "2px solid black"}}>{InlineComponent}</div>
+}
+
+function TextComponent(){
+  return <div>
+    hi there
   </div>
 }
-export default App
+
+export default App;
