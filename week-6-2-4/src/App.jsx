cdddd7baa024e5b50b7568874ceb1b2f95a8bb67
THAMITHA S <THAMITHA.S@ibm.com>
2024-12-08 19:04:36 +0530

    add inputfunction

--- week-6-2-4/src/App.jsx
+++ week-6-2-4/src/App.jsx
@@ -1,14 +1,17 @@
 import {useState, memo} from 'react'
 function App(){
   const [count, setCount] = useState(0)
+  function inputFunction(){
+    console.log('hi thier')
+  }
   return <div>
-  <ButtonComponent/>
+  <ButtonComponent initialFunction = {inputFunction}/>
   <button onClick={function(){
     setCount(count+1)
   }}>Counter {count}</button>
   </div>
 }
-const ButtonComponent= memo(function(){
+const ButtonComponent= memo(function({initialFunction}){
   console.log('child render')
   return <>
   <button>click me</button>
