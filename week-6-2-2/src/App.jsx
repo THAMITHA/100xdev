d4031476dcc788340ffe683d11983250383bb2f3
THAMITHA S <THAMITHA.S@ibm.com>
2024-12-08 11:57:49 +0530

    add changes

--- week-6-2-2/src/App.jsx
+++ week-6-2-2/src/App.jsx
@@ -1,16 +1,21 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 function App(){
   const [counter, setCounter] = useState(0)
   const [inputValue, setInputValue] = useState(1)
+  const [finalValue,setFinalValue] = useState(0)
   let count = 0
-  for(let i=1; i<=inputValue; i++){
-    count = count + i 
-  }
+  useEffect(()=>{
+    for(let i=1; i<=inputValue; i++){
+      count = count + i 
+    }
+    setFinalValue(count)
+  }, [inputValue])
+  
   return <>
     <input type="text" onChange = {(e)=>
       setInputValue(e.target.value)
     } placeholder='enter the number'></input><br/>
-    Sum for 1 to {inputValue} is {count}<br/>
+    Sum for 1 to {inputValue} is {finalValue}<br/>
     <button onClick={()=>
       setCounter(counter+1)
     }>Counter ({counter})</button>
