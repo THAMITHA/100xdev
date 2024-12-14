b007be332d08aaf473d62988a8d928a43c765613
THAMITHA S <THAMITHA.S@ibm.com>
2024-12-14 19:29:19 +0530

    add changes

--- assignments-master/week-6/1-use-memo/src/components/Assignment3.jsx
+++ assignments-master/week-6/1-use-memo/src/components/Assignment3.jsx
@@ -5,14 +5,21 @@ import React, { useState, useMemo } from 'react';
 export const Assignment3 = () => {
     const [items, setItems] = useState([
         { name: 'Chocolates', value: 10 },
-        { name: 'Chips', value: 20 },
+        { name: 'Chips', value: 30 },
         { name: 'Onion', value: 30 },
         { name: 'Tomato', value: 30 },
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(()=>{
+        let price = 0;
+        for(let i=0;i<items.length;i++){
+            price+=items[i].value
+        }
+        return price
+    },[items])
+
     // Your code ends here
     return (
         <div>
